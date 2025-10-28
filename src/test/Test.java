162307749466ee747ddfe4ad1daa76249f691da2
ElPunch <alfonsorobledo3548@gmail.com>
2025-10-27 20:42:27 -0600

    feat(storage): implement warehouse filling using time, category, and product classes

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
     public static void main(String[] args){
         //testCategory.run();
-        //testTime.run();
-        testProduct.run();
+        testTime.run();
+       // testProduct.run();
     }
 }
